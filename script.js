2586246201154edfc9342c262b40804a4767bc24
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-04-29 19:56:52 +0800

    added eraser function

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const DEFAULT_COLOR = '#000000';
 const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'pen';
 
 let currentColor = DEFAULT_COLOR;
 let currentSize = DEFAULT_SIZE;
+let eraserOn = false;
 
 function setCurrentColor(newColor) {
     currentColor = newColor;
@@ -12,29 +14,46 @@ function setCurrentSize(newSize) {
     currentSize = newSize;
 }
 
+function setEraser() {
+    if (!eraserOn) {
+        eraserOn = true;
+        eraser.style.backgroundColor = '#000000';
+        eraser.style.color = '#FFFFFF';
+    } else {
+        eraser.style.backgroundColor = '#FFFFFF';
+        eraser.style.color = '#000000';
+        eraserOn = false;
+    }
+}
+
+
 const color = document.getElementById('color');
 const clearBtn = document.getElementById('clearBtn');
 const sizeSlider = document.getElementById('box-size');
 const grid = document.getElementById('grid-container');
 const sizeText = document.getElementById('size-value');
-// const gridBorder = document.getElementById('grid-border');
 const gridBorder = document.getElementById('checkbox');
+const eraser = document.getElementById('eraser');
+
 
 clearBtn.onclick = () => reloadGrid();
 color.oninput = (e) => setCurrentColor(e.target.value);
 sizeSlider.onchange = (e) => changeSize(e.target.value);
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
 gridBorder.onclick = () => addGridBorder();
+eraser.onclick = () => setEraser();
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
+
 
 function changeSize(value) {
     setCurrentSize(value);
     updateSizeValue(value);
     reloadGrid();
 }
+
 
 function clearGrid() {
     grid.innerHTML = '';
@@ -62,7 +81,11 @@ function generateGrid(size) {
 
 function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return;
-    e.target.style.backgroundColor = currentColor;
+    if (!eraserOn) {
+        e.target.style.backgroundColor = currentColor;
+    } else {
+        e.target.style.backgroundColor = '#FFFFFF';
+    }
 }
 
 function updateSizeValue(e) {
